39e7a9533d087d9a0f4889197bfac0422b0d3f46
Iwao SUZUKI <iwao.suzuki@gmail.com>
2015-09-26 18:48:17 +0900

    Initial Commit

---
+++ ExpandNavigationBarSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpandNavigationBarSample
+//
+//  Created by Iwao SUZUKI on 2015/09/26.
+//  Copyright © 2015年 Iwao SUZUKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExpandNavigationBarSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExpandNavigationBarSample
+//
+//  Created by Iwao SUZUKI on 2015/09/26.
+//  Copyright © 2015年 Iwao SUZUKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
